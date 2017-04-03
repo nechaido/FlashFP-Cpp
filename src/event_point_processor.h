dc4926f267b7c2f38dd5abc8ede4d32e211bef89
Dmytro Nechai <nechaido@gmail.com>
2017-04-03 14:43:41 +0300

    src: specify `EventPointProcessor` interface

---
+++ src/event_point_processor.h
@@ -0,0 +1,25 @@
+#ifndef SRC_EVENT_POINT_PROCESSOR_H_
+#define SRC_EVENT_POINT_PROCESSOR_H_
+
+#include <list>
+
+#include "config.h"
+#include "audio_source.h"
+#include "fingerprint.h"
+
+namespace audio_recognition {
+
+class EventPointProcessor {
+ public:
+  EventPointProcessor(const Config* config);
+
+  std::list<FingerPrint> getFingerprints(AudioSource& audioSource);
+
+  void reser();
+ private:
+  const Config* config_;
+};
+
+} // namespace audio_recognition
+
+#endif //SRC_EVENT_POINT_PROCESSOR_H_
